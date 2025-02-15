b0baa6909796d193eb6f0372d323c2b13908233f
rgottardi <62321626+rgottardi@users.noreply.github.com>
2025-02-15 00:05:03 -0700

    Add admin layout

---
+++ app/(admin)/layout.tsx
@@ -0,0 +1,16 @@
+import { Header } from '@/components/shared/Header'
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1">
+        {children}
+      </main>
+    </div>
+  )
+}
